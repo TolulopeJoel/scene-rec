d57987bed96d188775228815e1d8f772129b4ac7
Tolu Joel <toluisjoel@gmail.com>
2024-08-03 19:28:41 +0100

    refactor: Update `get_best_match` to return multiple matching subtitles
    - Introduce `fetch_subtitles` function for optimized database querying with a limit.
    - Remove fuzzywuzzy dependency to simplify the code.

--- match.py
+++ match.py
@@ -1,15 +1,11 @@
-# TODO: expand `get_best_match` function to give user's options to choose from (simple fix)
 # TODO: implement function to split user input to phrases and iterate till all matches are found.
-
 import sqlite3
-
-from fuzzywuzzy import process
 
 from utilities import rinse_text
 
 
-def get_movie_details(result, cursor, matched_text):
-    movie_id = result[1]
+def get_movie_details(result, cursor):
+    matched_text, movie_id = result[0], result[1]
 
     movie_query = "SELECT name FROM movies WHERE id = ?"
     cursor.execute(movie_query, (movie_id,))
@@ -22,42 +18,39 @@ def get_movie_details(result, cursor, matched_text):
     return matched_text, movie_name, sub_start, sub_end
 
 
-def get_best_match(cursor, user_input):
+def fetch_subtitles(cursor, rinsed_text, limit=10):
+    search_query = """
+    SELECT text, movie_id, bm25(subtitles_fts) AS score 
+    FROM subtitles_fts WHERE text MATCH ?
+    ORDER BY score LIMIT ?
+    """
+    cursor.execute(search_query, (rinsed_text, limit))
+    return cursor.fetchall()
+
+
+def get_best_match(cursor, user_input, max_matches=10):
     """Find the best matching subtitle for the given user input."""
-    rinsed_text = rinse_text(user_input)
-
-    search_query = "SELECT text, movie_id FROM subtitles_fts WHERE text MATCH ?"
-    cursor.execute(search_query, (rinsed_text,))
-
-    if results := cursor.fetchall():
-        subtitles = [result[0] for result in results]
-
-        if best_matches := process.extractBests(rinsed_text, subtitles):
-            matched_text = best_matches[0][0]
-            for result in results:
-                if result[0] == matched_text:
-                    return get_movie_details(result, cursor, matched_text)
-    return (None, None, None, None)
+    if results := fetch_subtitles(cursor, rinse_text(user_input), max_matches):
+        return (get_movie_details(result, cursor) for result in results)
+    return ((None, None, None, None),)
 
 
-def main():
-    conn = sqlite3.connect("./subtitles.db")
-    cursor = conn.cursor()
+def main(cursor, user_input):
+    best_matches = get_best_match(cursor, user_input)
 
-    user_input = input("write... ")
-    best_text, movie_name, start, end = get_best_match(cursor, user_input)
-
-    if best_text:
-        print(f"\n> {user_input}")
-        print(f"Best matching text: {best_text}")
-        print(f"Movie name: {movie_name}")
-        print(f"Timestamp: {start} --> {end}")
-    else:
-        print("No matching subtitle found.")
-
-    conn.close()
+    print(f"\n> {user_input}")
+    for best_text, movie_name, start, end in best_matches:
+        if best_text:
+            print(f"\nBest matching text: {best_text}")
+            print(f"Movie name: {movie_name}")
+            print(f"Timestamp: {start} --> {end}")
+        else:
+            print("No matching subtitle found.")
 
 
 if __name__ == "__main__":
-    while True:
-        main()
+    conn = sqlite3.connect("./subtitles.db")
+    cursor = conn.cursor()
+    user_input = input("write... ")
+    main(cursor, user_input)
+    conn.close()
